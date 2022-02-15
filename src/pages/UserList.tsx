74a27d8ec0dccfbc6205bbaf9a8452c0e5a88468
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2022-02-15 15:15:19 +0100

    clean up & end tuto

--- src/pages/UserList.tsx
+++ src/pages/UserList.tsx
@@ -7,27 +7,33 @@ const UserList: React.FC = () => {
   const [listData, setListData] = useState<any>([]);
   useEffect(() => {
     let characterID = window.localStorage.characters
-    ? window.localStorage.characters.split(",")
-    : [];
+      ? window.localStorage.characters.split(",")
+      : [];
 
     for (let i = 0; i < characterID.length; i++) {
       axios
-      .get(
-        `https://rickandmortyapi.com/api/character/${characterID && characterID[i]}`
-      )
-      .then((res) => setListData((listData: any) => [...listData,res.data]))
+        .get(
+          `https://rickandmortyapi.com/api/character/${
+            characterID && characterID[i]
+          }`
+        )
+        .then((res) => setListData((listData: any) => [...listData, res.data]));
     }
-
-  }, [])
+  }, []);
   return (
-      <div className="user-list-page">
-              <Header />
-              <h2>List des personnages selectionnés</h2>
-              <div className="result">
-                {listData.length > 0 ? ( listData.map((character: any) => <Card character={character} key={character.id + `character`}/>)
-) : (<h2>Aucun personnages selectionnés</h2>)}
-              </div>
+    <div className="user-list-page">
+      <Header />
+      <h2>List des personnages selectionnés</h2>
+      <div className="result">
+        {listData.length > 0 ? (
+          listData.map((character: any) => (
+            <Card character={character} key={character.id + `character`} />
+          ))
+        ) : (
+          <h2>Aucun personnages selectionnés</h2>
+        )}
       </div>
+    </div>
   );
 };
 
